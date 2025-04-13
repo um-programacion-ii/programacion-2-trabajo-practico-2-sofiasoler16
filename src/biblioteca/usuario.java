02200bfd31d21163bb1ca1f1637c6166d3ed7e30
s.soler <s.soler@alumno.um.edu.ar>
2025-04-13 19:46:29 -0300

    Creado clase usuario con atributos, getters and setters

---
+++ src/biblioteca/usuario.java
@@ -0,0 +1,34 @@
+package biblioteca;
+
+public class usuario {
+    private String nombre;
+    private String apellido;
+    private String mail;
+
+    public usuario(String nombre, String apellido, String mail) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
