363668af2b00bbba725009c0706a5d2bbdece4fc
s.soler <s.soler@alumno.um.edu.ar>
2025-04-15 12:42:09 -0300

    Pruebo comportamiento consistente de subclases

--- src/biblioteca/Main.java
+++ src/biblioteca/Main.java
@@ -2,11 +2,6 @@ package biblioteca;
 
 public class Main {
     public static void main(String[] args) {
-        Podcast podcast = new Podcast("Vida de millonarios", 1, "LaCruda");
-
-        System.out.println("Título: " + podcast.getTitulo());
-        System.out.println("Canal: " + podcast.getCanal());
-
 
         Consola consola = new Consola();
 
@@ -26,5 +21,17 @@ public class Main {
         } else {
             System.out.println("Opción no implementada todavía.");
         }
+
+        RecursoDigitalBase libro = new Libro("Java en Acción", 1, "Editorial Sofi", "Sofía Soler", 2024);
+        RecursoDigitalBase revista = new Revista("Ciencia Hoy", 2, 34);
+        RecursoDigitalBase audiolibro = new AudioLibro("Historias que Inspiran", 3, "2:45");
+        RecursoDigitalBase podcast = new Podcast("Charlas Sofi", 4, "Canal Sofía");
+
+        System.out.println("=== Probar comportamiento consistente (LSP) ===");
+        libro.mostrarInformacion();
+        revista.mostrarInformacion();
+        audiolibro.mostrarInformacion();
+        podcast.mostrarInformacion();
+
     }
 }
