7616eaa138ece901666e70950e19a9311284d28d
s.soler <s.soler@alumno.um.edu.ar>
2025-04-19 19:32:36 -0300

    Agregada clase reserva

---
+++ src/biblioteca/Reserva.java
@@ -0,0 +1,33 @@
+package biblioteca;
+
+import java.time.LocalDateTime;
+
+public class Reserva {
+    private Usuario usuario;
+    private RecursoDigital recurso;
+    private LocalDateTime fechaReserva;
+
+    public Reserva(Usuario usuario, RecursoDigital recurso) {
+        this.usuario = usuario;
+        this.recurso = recurso;
+        this.fechaReserva = LocalDateTime.now();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public RecursoDigital getRecurso() {
+        return recurso;
+    }
+
+    public LocalDateTime getFechaReserva() {
+        return fechaReserva;
+    }
+
+    // lo que imprime cuando llamo al  objeto
+    @Override
+    public String toString() {
+        return "Reserva de " + recurso.getIdentificador() + "por " + fechaReserva
+    }
+}
