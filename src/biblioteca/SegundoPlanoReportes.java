fc00ff13ce7b192ebeb9553dffd28a78d393a0e4
s.soler <s.soler@alumno.um.edu.ar>
2025-04-22 18:25:29 -0300

    Creada clase SegundoPlanoReportes

---
+++ src/biblioteca/SegundoPlanoReportes.java
@@ -0,0 +1,38 @@
+package biblioteca;
+
+public class SegundoPlanoReportes implements Runnable{
+    private final ServicioReportes servicioReportes;
+    private final String tipo;
+
+    public SegundoPlanoReportes(ServicioReportes servicioReportes, String tipo) {
+        this.servicioReportes = servicioReportes;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("🔍 Generando reporte: " + tipo + "...");
+        try {
+            Thread.sleep(3000); // Simula procesamiento
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        switch (tipo) {
+            case "prestamos":
+                servicioReportes.reporteRecursosMasPrestados(5);
+                break;
+            case "usuarios":
+                servicioReportes.reporteUsuariosMasActivos(5);
+                break;
+            case "categorias":
+                servicioReportes.reporteEstadisticasPorCategoria();
+                break;
+            default:
+                System.out.println("Tipo de reporte desconocido.");
+        }
+
+        System.out.println("✅ Reporte " + tipo + " generado.");
+    }
+
+}
